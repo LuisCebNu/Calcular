791c7d97aaebcbd79c2ab6ecc06890b0eeff614c
Luis Ceballo <66170740+SonicRushFan@users.noreply.github.com>
2022-04-12 18:23:16 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Calcular.Models;
+
+namespace Calcular.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(calculator ca, string calculate)
+        {
+            switch(calculate)
+            {
+                case "Add":
+                 {
+                        ca.result = ca.num1 + ca.num2;
+                        break;
+                 }
+
+                case "Subs":
+                    {
+                        ca.result = ca.num1 - ca.num2;
+                        break;
+                    }
+                case "Multiplie":
+                    {
+                        ca.result = ca.num1 * ca.num2;
+                        break;
+                    }
+                case "Division":
+                    {
+                        ca.result = ca.num1 / ca.num2;
+                        break;
+                    }
+            }
+
+            ViewBag.previus_results = ca.result;
+            return View(ca);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calcular.Models
+{
+    public class calculator
+    {
+        //Variables
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int result { get; set; }
+    }
+}
